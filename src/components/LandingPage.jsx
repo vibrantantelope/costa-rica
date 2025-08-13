82cb3a97e16856d274759543a72b7cc8f0034ea3
John Kenny <167453693+jvkenny@users.noreply.github.com>
2025-08-13 14:37:38 -0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,10 +7,7 @@ export default function LandingPage({ onEnter, onShowTips, onShowRules }) {
       <div className="landing">
         <h1 className="site-title">Costa Rica Trip 2025</h1>
         <p className="site-subtitle">
-          So excited for this trip together! These schedules were built from everyone's responses to the activity planning sheet.
-        </p>
-        <p className="site-explainer">
-          These schedules were generated from everyone's activity preferences and are offered in three tabbed options.
+          So excited for this trip together! To make sure we all have a great time, we've put together this website with the activity schedule we sent to the concierge, tips and coordination information, and rules and expectations of us some items to help our group have a great time!
         </p>
 
         {/* New horizontal button row */}
